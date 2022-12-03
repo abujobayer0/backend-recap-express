5136466a0803a3652eada8fc22d68b69a86980a4
zubayermunna <muna700064@gmail.com>
2022-12-03 10:21:14 +0600

    added steps

---
+++ _steps.js
@@ -0,0 +1,28 @@
+/**
+ * Basic server setup
+ * -----------
+ * create a folder (manually or mkdir)
+ * open the folder in command line
+ * npm init
+ * npm install express
+ * create index.js
+ *
+ * ----------------
+ * For Express
+ * ----------
+ * requre express
+ * create app cariable
+ * dexclare port
+ * set app.get('/)
+ * listen to port
+ * node index.js
+ * check your browser for that port
+ * ------------
+ * post
+ * ---------
+ *
+ *
+ * ---------get----------
+ * cors middlewire
+ * -----------------
+ */
